33d150afb6b2ad76092c9d715bdf5ed244e56ff5
SABIR ALI <sabirsheik12787@gmail.com>
2024-03-21 20:08:39 +0500

    a

--- src/Components/main/mainComp.jsx
+++ src/Components/main/mainComp.jsx
@@ -12,7 +12,7 @@ export default function Main() {
       <div className="about-me">
        <div className="left-main">
         <div className="left-main-img">
-        <img src="https://photos.app.goo.gl/FfLspixYCXapAuzJ7" alt="" />
+        <img src="https://lh3.googleusercontent.com/pw/AP1GczMSQbUa-WLBgdQA7noNgUmlIMhTLIaeBhJuF4e9VyNEcteSEgWag0IKnwTYUHv2OAdqcbX4hq0JJPMoUI5468jnN5Bc0Ol6pBb4GVeNFRmqSquJq6J_NsPtFaRxDo61KZImyp2i1RuRcldlrpED6rV3AzCrbBKH1ajlst_m-Q3bpVcQ0qKz_JePL1W98-WLyt5MBOAsWyShFvmaeT2x8eDaiSnnrzIuav6JTn3Wp9X3CKvbyzulYW-_9Kz8V786o7cY5yXwWtcjC2ug8SPWzdNkfqWArcZKbRqDMPixXZl8bSAKTpqctQ-QpbnHh-BRIrNXNBfqIwP1Jo9BTq8U1T89mpHTzLZTAxel31PZFzXgDC2wCTRMkHORFQIBoadHEcmj-A_WdLFAJUbZusOOmz_yfvumX_rZgMl6lvaVivQUxpLMM0x20eElVCEeSMD5bWD2LV2ftCeTPgl0V0weqd-HCkrItDZgvpx7ABeoTxIoTihyPVGdeVVlJ9fkTB4VCWsn4YdIYrHIXIiPyGn8gTlBMPQqLDDGgGwnr82yBiq1pV0-o7mZwoPlOAXnASRmmNj90v2WAYj_SR1Zg_vfnbSNzcPpX6ZHA-8e-utJ2omROz0J5dnkJFcJgFMmSPL8Bnf61waKDOhIC9qIA11uTTF-bjDQi3uXRw2yd06_GHeJig0vIFbpi2P_EgnSsm0GmkhV8tibfaBjXYNKPI0fQEJypR4mJ6qdRalaMlDfeum5Lr0zXilMTSdx27j5FDAVUqPRe0KrCpw_bqiUVvov1xqh38poH2hZGZXFQ-90yalxhp2_-eN9_JZZV9D6TbTZQh1B0KEtc-A5QzQKhgktw0OjiHWMgvUY4YAWJoUiBgnXkPv4lY57bQ2xidPnJBo95VHGvO3k1NTC4chjqrgrDEbyfc8=w297-h272-s-no-gm?authuser=0" alt="" />
         </div>
        </div>
 
