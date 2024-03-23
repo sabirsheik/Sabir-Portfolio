fa0cbe33ea67dc796a0162e0234d2133c4c99e74
SABIR ALI <sabirsheik12787@gmail.com>
2024-03-24 02:04:02 +0500

    minor chg

--- src/Components/main/mainComp.jsx
+++ src/Components/main/mainComp.jsx
@@ -2,38 +2,38 @@ import React from 'react'
 import "./main.css"
 import "./main.js"
 export default function Main() {
-  
+
   return (
-   <>
-    <div className="container-main">
+    <>
+      <div className="container-main">
         <h1 className='h'>About Me</h1>
-    <main className='reveal'>
-      {/* About Me */}
-      <div className="about-me">
-       <div className="left-main">
-        <div className="left-main-img">
-        <img src="https://lh3.googleusercontent.com/pw/AP1GczMSQbUa-WLBgdQA7noNgUmlIMhTLIaeBhJuF4e9VyNEcteSEgWag0IKnwTYUHv2OAdqcbX4hq0JJPMoUI5468jnN5Bc0Ol6pBb4GVeNFRmqSquJq6J_NsPtFaRxDo61KZImyp2i1RuRcldlrpED6rV3AzCrbBKH1ajlst_m-Q3bpVcQ0qKz_JePL1W98-WLyt5MBOAsWyShFvmaeT2x8eDaiSnnrzIuav6JTn3Wp9X3CKvbyzulYW-_9Kz8V786o7cY5yXwWtcjC2ug8SPWzdNkfqWArcZKbRqDMPixXZl8bSAKTpqctQ-QpbnHh-BRIrNXNBfqIwP1Jo9BTq8U1T89mpHTzLZTAxel31PZFzXgDC2wCTRMkHORFQIBoadHEcmj-A_WdLFAJUbZusOOmz_yfvumX_rZgMl6lvaVivQUxpLMM0x20eElVCEeSMD5bWD2LV2ftCeTPgl0V0weqd-HCkrItDZgvpx7ABeoTxIoTihyPVGdeVVlJ9fkTB4VCWsn4YdIYrHIXIiPyGn8gTlBMPQqLDDGgGwnr82yBiq1pV0-o7mZwoPlOAXnASRmmNj90v2WAYj_SR1Zg_vfnbSNzcPpX6ZHA-8e-utJ2omROz0J5dnkJFcJgFMmSPL8Bnf61waKDOhIC9qIA11uTTF-bjDQi3uXRw2yd06_GHeJig0vIFbpi2P_EgnSsm0GmkhV8tibfaBjXYNKPI0fQEJypR4mJ6qdRalaMlDfeum5Lr0zXilMTSdx27j5FDAVUqPRe0KrCpw_bqiUVvov1xqh38poH2hZGZXFQ-90yalxhp2_-eN9_JZZV9D6TbTZQh1B0KEtc-A5QzQKhgktw0OjiHWMgvUY4YAWJoUiBgnXkPv4lY57bQ2xidPnJBo95VHGvO3k1NTC4chjqrgrDEbyfc8=w297-h272-s-no-gm?authuser=0" alt="" />
-        </div>
-       </div>
+        <main className='reveal'>
+          {/* About Me */}
+          <div className="about-me">
+            <div className="left-main">
+              <div className="left-main-img">
+                <img src="https://lh3.googleusercontent.com/pw/AP1GczMSQbUa-WLBgdQA7noNgUmlIMhTLIaeBhJuF4e9VyNEcteSEgWag0IKnwTYUHv2OAdqcbX4hq0JJPMoUI5468jnN5Bc0Ol6pBb4GVeNFRmqSquJq6J_NsPtFaRxDo61KZImyp2i1RuRcldlrpED6rV3AzCrbBKH1ajlst_m-Q3bpVcQ0qKz_JePL1W98-WLyt5MBOAsWyShFvmaeT2x8eDaiSnnrzIuav6JTn3Wp9X3CKvbyzulYW-_9Kz8V786o7cY5yXwWtcjC2ug8SPWzdNkfqWArcZKbRqDMPixXZl8bSAKTpqctQ-QpbnHh-BRIrNXNBfqIwP1Jo9BTq8U1T89mpHTzLZTAxel31PZFzXgDC2wCTRMkHORFQIBoadHEcmj-A_WdLFAJUbZusOOmz_yfvumX_rZgMl6lvaVivQUxpLMM0x20eElVCEeSMD5bWD2LV2ftCeTPgl0V0weqd-HCkrItDZgvpx7ABeoTxIoTihyPVGdeVVlJ9fkTB4VCWsn4YdIYrHIXIiPyGn8gTlBMPQqLDDGgGwnr82yBiq1pV0-o7mZwoPlOAXnASRmmNj90v2WAYj_SR1Zg_vfnbSNzcPpX6ZHA-8e-utJ2omROz0J5dnkJFcJgFMmSPL8Bnf61waKDOhIC9qIA11uTTF-bjDQi3uXRw2yd06_GHeJig0vIFbpi2P_EgnSsm0GmkhV8tibfaBjXYNKPI0fQEJypR4mJ6qdRalaMlDfeum5Lr0zXilMTSdx27j5FDAVUqPRe0KrCpw_bqiUVvov1xqh38poH2hZGZXFQ-90yalxhp2_-eN9_JZZV9D6TbTZQh1B0KEtc-A5QzQKhgktw0OjiHWMgvUY4YAWJoUiBgnXkPv4lY57bQ2xidPnJBo95VHGvO3k1NTC4chjqrgrDEbyfc8=w297-h272-s-no-gm?authuser=0" alt="" />
+              </div>
+            </div>
 
-       <div className="right-main">
-        <h1>Hi There I'm <span>Sabir ALi</span></h1>
-        <h3>Web Devolper</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati enim nobis debitis nesciunt, tempora nulla? Vel ad laborum, fuga vitae illum nesciunt!
-        </p>
+            <div className="right-main">
+              <h1>Hi There I'm <span>Sabir ALi</span></h1>
+              <h3>Web Devolper</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati enim nobis debitis nesciunt, tempora nulla? Vel ad laborum, fuga vitae illum nesciunt!
+              </p>
 
-        <button className='Down'>Download CV</button>
-       </div>
+              <button className='Down'>Download CV</button>
+            </div>
+          </div>
+        </main>
       </div>
-   </main>
-    </div>
 
-          {/* Services */}
-     
-     {/*Extenal File  */}
-   
-      
+      {/* Services */}
 
-   </>
+      {/*Extenal File  */}
+
+
+
+    </>
   )
 }
