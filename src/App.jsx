38669b9c10f31c0669f08946a69ac27dcb8eaa66
SABIR ALI <sabirsheik12787@gmail.com>
2024-03-31 15:10:34 +0500

    chg

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import "./App.css"
+// import "./App.css"
 import Header from "./Components/header/header"
 import Main from "./Components/main/mainComp"
 import { product } from "./data/product"
