a1b6a528bca27f61f39c88421d44bcf911c549ee
SABIR ALI <sabirsheik12787@gmail.com>
2024-05-10 02:10:49 +0500

    Additional Adding And removing

--- src/data/product.js
+++ src/data/product.js
@@ -11,7 +11,7 @@ export let product = [
         img : "https://images.unsplash.com/photo-1457305237443-44c3d5a30b89?q=80&w=874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         title: "E Commerecs",
         statement: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste eius!",
-        link:'https://01-the-shoe-company.vercel.app/#hero'
+        link:'Coming Soon'
 
     },
     {
@@ -19,27 +19,27 @@ export let product = [
         img : "https://images.unsplash.com/photo-1461749280684-dccba630e2f6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHdlYiUyMGRldm9scGVyfGVufDB8fDB8fHww",
         title: "Shopify Store",
         statement: "Lorem ipsum dolor sit, amet consectetur adipisicing elit eius!",
-        link:'https://01-the-shoe-company.vercel.app/#hero',
+        link:'Coming Soon',
     },
     {
         id: 4,
         img : "https://images.unsplash.com/photo-1583339793403-3d9b001b6008?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aHRtbCUyRmNzc3xlbnwwfHwwfHx8MA%3D%3D",
         title: "Portfolio",
         statement: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
-        link:'https://01-the-shoe-company.vercel.app/#hero'
+        link:'Coming Soon'
     },
     {
         id: 5,
         img : "https://images.unsplash.com/photo-1592609931041-40265b692757?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fG5vZGUlMjBqc3xlbnwwfHwwfHx8MA%3D%3D",
         title: "React Application",
         statement: "Lorem ipsum, amet consectetur adipisicing elit. Iste ducimus reiciendis eius!",
-        link:'https://01-the-shoe-company.vercel.app/#hero'
+        link:'Comming Soon'
     },
     {
         id: 6,
         img : "https://images.unsplash.com/photo-1537432376769-00f5c2f4c8d2?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHByb2dyYW1taW5nfGVufDB8fDB8fHww",
         title: "Web Application",
         statement: "Lorem ipsum dolor sit, eiciendis eius!",
-        link:'https://01-the-shoe-company.vercel.app/#hero'
+        link:'Coming Soon'
     }
 ]
